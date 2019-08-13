391519c5dc3252e4b7cb3f0920cc23e6be26f739
Till <thoffmann@mail.hs-ulm.de>
2019-08-13 14:00:36 +0200

    added apt-get update after adding docker-repo

--- install-docker.sh
+++ install-docker.sh
@@ -16,5 +16,6 @@ apt-get install apt-transport ca-certificates curl gnupg-agent software-properti
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-get add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   # $(lsb_release -cs) returns for example "xenial"
+apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
   # about 390 MB
