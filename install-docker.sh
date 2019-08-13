eded751b108321e6e16f81ae3e3577353293cefe
Till <thoffmann@mail.hs-ulm.de>
2019-08-13 14:03:45 +0200

    fixed package name

--- install-docker.sh
+++ install-docker.sh
@@ -12,7 +12,7 @@ apt-get clean -y
 ### install software
 
 ## docker
-apt-get install apt-transport ca-certificates curl gnupg-agent software-properties-common
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-get add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   # $(lsb_release -cs) returns for example "xenial"
