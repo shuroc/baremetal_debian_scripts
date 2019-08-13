6bae79cded5788517bc9e7fa16caf2769f486ec4
Till <thoffmann@mail.hs-ulm.de>
2019-08-13 14:06:42 +0200

    splitted install-docker into install-docker and update-system

--- install-docker.sh
+++ install-docker.sh
@@ -2,20 +2,13 @@
 #   chmod +x ./install.sh
 # does the trick
 
-### update system
-
-apt-get update && \
-apt-get upgrade -y && \
-apt-get clean -y
-
 
 ### install software
-
 ## docker
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-get add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   # $(lsb_release -cs) returns for example "xenial"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
   # about 390 MB
---
+++ update-system.sh
@@ -0,0 +1,9 @@
+# to run this script you must first allow execution
+#   chmod +x ./install.sh
+# does the trick
+
+
+### update system
+apt-get update && \
+apt-get upgrade -y && \
+apt-get clean -y
