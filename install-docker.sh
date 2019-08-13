3064500e93424f9dcafa1ad081f72a88b605e641
Till <thoffmann@mail.hs-ulm.de>
2019-08-13 14:13:39 +0200

    fixed typo

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,7 @@
 ### install software
 ## docker
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-get add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   # $(lsb_release -cs) returns for example "xenial"
 apt-get update
