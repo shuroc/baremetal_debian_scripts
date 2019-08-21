060fcd2d0910e576363b268e21bf951eed10bdfb
shuroc <25052289+shuroc@users.noreply.github.com>
2019-08-21 17:43:52 +0200

    modified for raspbian on raspberry3b

--- docker-install.sh
+++ docker-install.sh
@@ -6,9 +6,8 @@
 ### install software
 ## docker
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/raspbian $(lsb_release -cs) stable"
   # $(lsb_release -cs) returns for example "xenial"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-  # about 390 MB
