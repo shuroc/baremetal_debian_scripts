cc0bb24d3d83232230017600ac8dab0eb1c39d11
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-03 01:52:47 +0200

    fixing minor problems

--- kubernetes-install.sh
+++ kubernetes-install.sh
@@ -6,7 +6,7 @@ POD_NETWORK_CIDR="10.244.0.0/16"
 
 # configuring container runtime
 # https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-echo "Make sure you have docker installed..." && sleep 3
+echo "Please make sure you have docker installed!" && sleep 3
 printf '{
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -14,7 +14,7 @@ printf '{
     "max-size": "100m"
   },
   "storage-driver": "overlay2"
-}' | sudo tee /etc/docker/daemon.json
+}' | sudo tee /etc/docker/daemon.json >/dev/null
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo systemctl daemon-reload
 sudo systemctl restart docker
@@ -24,21 +24,21 @@ sudo systemctl enable docker
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#letting-iptables-see-bridged-traffic
 sudo modprobe br_netfilter
 printf "net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1" | sudo tee /etc/sysctl.d/k8s.conf >/dev/null
 sudo sysctl --system
 
 # Installing kubeadm, kubelet and kubectl
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-printf "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+printf "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Initializing your control-plane node
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#initializing-your-control-plane-node
-if [ $(grep "$CONTROL_PLANE_ENDPOINT" /etc/hosts) = "" ]; then
+if [ "$(grep '$CONTROL_PLANE_ENDPOINT' /etc/hosts)" = "" ]; then
   printf "%-20s%s" "127.0.0.1" "$CONTROL_PLANE_ENDPOINT" | sudo tee -a /etc/hosts >/dev/null
 fi
 sudo kubeadm init --control-plane-endpoint $CONTROL_PLANE_ENDPOINT --pod-network-cidr $POD_NETWORK_CIDR
@@ -56,7 +56,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 wget https://docs.projectcalico.org/manifests/tigera-operator.yaml -O calico-tigera-operator.yaml
 kubectl apply -f calico-tigera-operator.yaml
 wget https://docs.projectcalico.org/manifests/custom-resources.yaml -O calico-custom-resources.yaml
-sed -i "s/cidr: .*$/cidr: $POD_NETWORK_CIDR/" calico-custom-resources.yaml
+sed -i "s|cidr: .*$|cidr: $POD_NETWORK_CIDR|" calico-custom-resources.yaml
 kubectl apply -f https://docs.projectcalico.org/manifests/custom-resources.yaml
 curl -O -L https://github.com/projectcalico/calicoctl/releases/download/v3.16.1/calicoctl
 chmod +x calicoctl
