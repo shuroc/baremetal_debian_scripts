7705597b49a98e0fbd1d30881bb70a9531ae4a66
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-29 03:34:52 +0100

    fixed transcrypt installation

--- install-transcrypt.sh
+++ install-transcrypt.sh
@@ -14,9 +14,8 @@ if [ "$installed_version" == "" ] || [ "$installed_version" != "$version" ]; the
   # on first installation and
   # on version changes, too
 
-  curl -L https://golang.org/dl/go$version.linux-amd64.tar.gz -o ./go.tar.gz && sudo tar -C /usr/local -xzf ./go.tar.gz && rm ./go.tar.gz
-  git clone https://github.com/elasticdog/transcrypt.git
-  mv transcrypt/transcrypt /usr/local/bin/transcrypt
+  git clone --singlebranch --branch v$version https://github.com/elasticdog/transcrypt.git
+  chmod +x transcrypt/transcrypt
+  sudo mv transcrypt/transcrypt /usr/local/bin/transcrypt
   rm -drf ./transcrypt
-  chmod +x /usr/local/bin/transcrypt
 fi
