5c0f10acd61a9c4d0271534514d376395853efe6
Till Hoffmann <till.hoffmann@enforge.de>
2020-10-29 01:51:43 +0100

    added docker-compose installation script

---
+++ install-docker-compose.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID = 0 ]]; then
+  <&2 echo "You're root. This script isn't meant for that."
+  exit
+fi
+
+version=1.27.4
+
+
+installed_version=$(docker-compose version | grep docker-compose | cut -d' ' -f3 | cut -d',' -f1)
+installcheck=$?
+
+if [ $installcheck -ne 0 -or "$installed_version" != "$version" ]; then
+  # on first installation and
+  # on version changes, too
+
+  sudo curl -L "https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
