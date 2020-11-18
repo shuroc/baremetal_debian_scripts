599bc2afb4545c35d1faf0cf456dd61145f340f7
Till Hoffmann <till.hoffmann@enforge.de>
2020-11-18 18:50:25 +0100

    improved readability of install-zfs

--- install-zfs.sh
+++ install-zfs.sh
@@ -5,14 +5,16 @@ if [[ $EUID -eq 0 ]]; then
   exit
 fi
 
-echo -e "deb http://deb.debian.org/debian buster-backports main contrib
-deb-src http://deb.debian.org/debian buster-backports main contrib
-" | sudo tee /etc/apt/sources.list.d/buster-backports.list
+printf "%s\n" \
+  "deb http://deb.debian.org/debian buster-backports main contrib" \
+  "deb-src http://deb.debian.org/debian buster-backports main contrib" \
+  | sudo tee /etc/apt/sources.list.d/buster-backports.list >/dev/null
 
-echo -e "Package: libnvpair1linux libuutil1linux libzfs2linux libzpool2linux spl-dkms zfs-dkms zfs-test zfsutils-linux zfsutils-linux-dev zfs-zed
-Pin: release n=buster-backports
-Pin-Priority: 990
-" | sudo tee /etc/apt/preferences.d/90_zfs
+printf "%s\n" \
+  "Package: libnvpair1linux libuutil1linux libzfs2linux libzpool2linux spl-dkms zfs-dkms zfs-test zfsutils-linux zfsutils-linux-dev zfs-zed" \
+  "Pin: release n=buster-backports" \
+  "Pin-Priority: 990" \
+  | sudo tee /etc/apt/preferences.d/90_zfs >/dev/null
 
 sudo apt update
 sudo apt install -y dpkg-dev linux-headers-$(uname -r) linux-image-amd64 debootstrap gdisk dkms
