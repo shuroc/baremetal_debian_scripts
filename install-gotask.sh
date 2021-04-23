bc0e293011cdab0e62a735ddd6a2972a2f6f6a22
tillhoff <till.hoffmann@enforge.de>
2021-04-24 00:14:56 +0200

    just use the installer script

--- install-gotask.sh
+++ install-gotask.sh
@@ -5,13 +5,16 @@ if [[ $EUID -eq 0 ]]; then
   exit
 fi
 
-version=3.0.0
+# version=3.0.0
 
-installed_version=$((task --version | cut -d' ' -f3) 2>/dev/null)
+# installed_version=$((task --version | cut -d' ' -f3) 2>/dev/null)
 
-if [ "$installed_version" == "" ] || [ "$installed_version" != "$version" ]; then
-  # on first installation and
-  # on version changes, too
+# if [ "$installed_version" == "" ] || [ "$installed_version" != "$version" ]; then
+#   # on first installation and
+#   # on version changes, too
   
-  curl -L "https://github.com/go-task/task/releases/download/v$version/task_linux_$(dpkg --print-architecture).deb" -o ./go-task.deb && sudo dpkg -i ./go-task.deb && sudo rm ./go-task.deb    
-fi
+#   curl -L "https://github.com/go-task/task/releases/download/v$version/task_linux_$(dpkg --print-architecture).deb" -o ./go-task.deb && sudo dpkg -i ./go-task.deb && sudo rm ./go-task.deb    
+# fi
+
+# Why the hassle - just take the installer script
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
